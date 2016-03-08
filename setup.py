42ef491061c3790307d39356b307c4318032c439
Paul Decoursey <paul@decoursey.net>
2016-03-08 10:15:37 -0600

    version bump

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ THE SOFTWARE.
 
 setup(
     name='redlock-py',
-    version='1.0.7',
+    version='1.0.8',
     packages=find_packages(),
     include_package_data=True,
     description='Redis locking mechanism',
